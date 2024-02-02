889c90056226baedb84bb7dfc7f7171f8fd44e16
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2024-02-02 16:48:29 +0000

    configuracion del proyecto

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import express from "express"
 const app=express()
 
+
+app.get("/employee",(req,res)=>{
+    res.send("Employee details")
+})
+app.post("/employee",(req,res)=>{
+    res.send("Employee details")
+})
+
+app.put("/employee",(req,res)=>{
+    res.send("Employee details")
+})
+
+app.delete("/employee",(req,res)=>{
+    res.send("Employee details")
+})
+
 app.listen(3000,()=>{
     console.log("Server is running at port 3000")
 })
